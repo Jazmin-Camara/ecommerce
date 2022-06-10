94ebc7f7bd4dc8a32e0fbcaf5ff0148845b21430
Jazmin-Camara <jazmincamara@hotmail.com>
2022-06-09 22:44:19 -0500

    Se agrego darkmode

---
+++ js/main.js
@@ -0,0 +1,17 @@
+"use strict"
+
+
+let themeIcon = document.getElementById("theme-toggler")
+let body = document.querySelector("body")
+
+themeIcon.addEventListener( "click", (e) =>{
+    body.classList.toggle("dark-theme")
+    
+    let isDark = body.classList.contains("dark-theme")
+
+    if(isDark){
+        themeIcon.classList.replace("bx-moon", "bx-sun")
+    }else{
+        themeIcon.classList.replace("bx-sun", "bx-moon")
+    }
+})
