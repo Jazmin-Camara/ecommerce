3e528c66862463d4703d43f5f28ab8c9d1e1d8a2
Jazmin-Camara <jazmincamara@hotmail.com>
2022-06-13 16:13:20 -0500

    Maquetado terminado

--- js/main.js
+++ js/main.js
@@ -17,7 +17,41 @@ themeIcon.addEventListener( "click", (e) =>{
 })
 
 
-/*Local Storage*/
-/*Creacion de informacion*/
 
-window.localStorage.setItem( "  ")
+
+
+const items = [
+    {
+      id: 1,
+      name: 'Hoodies',
+      price: 14.00,
+      image: 'https://academlo-store.netlify.app/assets/img/featured1.png',
+      category: 'hoodies',
+      quantity: 10
+    },
+    {
+      id: 2,
+      name: 'Shirts',
+      price: 24.00,
+      image: 'https://academlo-store.netlify.app/assets/img/featured2.png',
+      category: 'shirts',
+      quantity: 15
+    },
+    {
+      id: 3,
+      name: 'Sweatshirts',
+      price: 24.00,
+      image: 'https://academlo-store.netlify.app/assets/img/featured3.png',
+      category: 'sweatshirts',
+      quantity: 20
+    },
+    {
+      id: 4,
+      name: 'Sweatshirts',
+      price: 30.00,
+      image: 'https://academlo-store.netlify.app/assets/img/featured3.png',
+      category: 'sweatshirts',
+      quantity: 10
+    }
+]
+
